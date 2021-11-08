980662402c7d2173e9ca40745cdb550e508689b4
vsipra <vsipra@users.noreply.github.com>
2021-11-07 21:16:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 #Create the app object
 app = FastAPI()
 
-# Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
     return {'message': 'Hello, World'}
