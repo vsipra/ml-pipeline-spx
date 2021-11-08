8849d89b7f51191c9c888ce99180eda125aac918
vsipra <vsipra@users.noreply.github.com>
2021-11-07 21:13:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def index():
     return {'message': 'Hello, World'}
 
 # Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
+#    Located at: http://0.0.0.0:8000/AnyNameHere
 @app.get('/Welcome')
 def get_name(name: str):
     return {'Welcome To Vajiha ML model prediction': f'{name}'}
 
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
+# Run the API with uvicorn
+#    Will run on http://0.0.0.0:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
